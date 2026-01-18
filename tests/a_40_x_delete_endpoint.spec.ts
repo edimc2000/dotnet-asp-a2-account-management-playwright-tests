83f08ad24dc6414f15fcf3a9adc4707884d2bf67
Eddie C. <edimc2000@gmail.com>
2026-01-18 01:05:49 -0600

    completed set 40 test suite

--- tests/a_40_x_delete_endpoint.spec.ts
+++ tests/a_40_x_delete_endpoint.spec.ts
@@ -8,7 +8,7 @@ test.describe.configure({ mode: 'serial' });
 //These test are dependent on 20_x test package which registers accounts 204, 205 and 206
 test.describe('Delete Endpoint', () => {
 
-    test('TC 40: Delete accounts 204, 205, 206',
+    test('TC 40: HTTP 200: Delete registered accounts 204, 205, 206',
         async ({ request }) => {
             let ids = [204, 205, 206]
 
@@ -29,11 +29,57 @@ test.describe('Delete Endpoint', () => {
                 expect.soft(response.statusText()).toBe('OK')
                 expect.soft(responseData.success).toBe(true)
                 expect.soft(responseData.message).toBe('Account deleted successfully')
+            }
+        })
+
+    test('TC 41: HTTP 400: Delete invalid accounts 204a, 205a, 206a (string input - unparsable)',
+        async ({ request }) => {
+            let ids = ['204a', '205a', '206a']
+
+            for (let index = 0; index < ids.length; index++) {
+                const id = ids[index]
+
+                const endpoint = `${part2BaseURL}${deleteUsingIdEndpointUrl}${id}`
+                const response = await request.delete(endpoint)
+                const responseData = await response.json();
+
+                divider()
+                console.log('Response Status:', response.status())
+                console.log('Response Status Text:', response.statusText())
+                console.log('Response Body:', responseData)
+
+                // assertions
+                expect.soft(response.status()).toBe(400)
+                expect.soft(response.statusText()).toBe('Bad Request')
+                expect.soft(responseData.success).toBe(false)
+                expect.soft(responseData.message).toBe(`'${id}' is not a valid account Id`)
 
 
             }
         })
+    test.only('TC 42: HTTP 400: Delete restricted accounts 200, 201, 202, 203',
+        async ({ request }) => {
+            let ids = [200, 201, 202, 203]
 
+            for (let index = 0; index < ids.length; index++) {
+                const id = ids[index]
+
+                const endpoint = `${part2BaseURL}${deleteUsingIdEndpointUrl}${id}`
+                const response = await request.delete(endpoint)
+                const responseData = await response.json();
+
+                divider()
+                console.log('Response Status:', response.status())
+                console.log('Response Status Text:', response.statusText())
+                console.log('Response Body:', responseData)
+
+                // assertions
+                expect.soft(response.status()).toBe(400)
+                expect.soft(response.statusText()).toBe('Bad Request')
+                expect.soft(responseData.success).toBe(false)
+                expect.soft(responseData.message).toBe(`Account ID '${id}' is restricted and cannot be deleted`)
+            }
+        })
 
 
 })
